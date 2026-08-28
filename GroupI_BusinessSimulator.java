1cf1dff2c185937e5e9fb108215714d66e5f1f18
EvelynTris <214513409+EvelynTris@users.noreply.github.com>
2026-08-28 21:57:02 +0300

    Evelyn: updated file

--- GroupI_BusinessSimulator.java
+++ GroupI_BusinessSimulator.java
@@ -80,14 +80,7 @@ public class GroupI_BusinessSimulator {
             );
         }
 
-        // Calculate grand total
-        double grandTotal = 0;
-
-        for (int i = 0; i < items.length; i++) {
-            grandTotal += totals[i];
-        }
-
-                // Display receipt (Evelyn's work)
+            // Display receipt 
         System.out.println();
         System.out.println("========== RECEIPT ==========");
         double grandTotal = 0.0;
